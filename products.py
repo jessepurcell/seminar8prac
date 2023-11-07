32ad1172a96227e634c36cd043a948f6baf29ee9
Jesse Purcell <slaydevil@hotmail.com>
2023-11-07 20:20:08 +1000

    Refactor

--- products.py
+++ products.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.button import Button
 
 
 class Products(App):
@@ -7,14 +8,23 @@ class Products(App):
         super().__init__(**kwargs)
         self.total_price = 0
         self.products = [Product("Test", 1)]
-        # load products here
 
     def build(self):
-        self.title = "Hello world!"
+        self.title = "Products"
         self.root = Builder.load_file('products.kv')
+        self.create_widgets()
         return self.root
 
-    def handle_reset(self, button):
+    def create_widgets(self):
+        """Create buttons from data and add them to the GUI."""
+        for product in self.products:
+            # create a button for each data entry, specifying the text
+            temp_button = Button(text=product)
+            temp_button.bind(on_press=self.handle_button)
+            # add the button to the "entries_box" layout widget
+            self.root.ids.products.add_widget(temp_button)
+
+    def handle_reset(self):
         self.total_price = 0
 
     def handle_button(self, button):
